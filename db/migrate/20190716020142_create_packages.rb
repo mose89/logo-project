c959edac501ee9893e83e6e4138f4fa9ab5c708c
Mose Gebreselassie <mose89@gmail.com>
2019-07-16 04:08:54 +0200

    Create Package Model with attributes

---
+++ db/migrate/20190716020142_create_packages.rb
@@ -0,0 +1,20 @@
+class CreatePackages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :packages do |t|
+      t.boolean :logo
+      t.boolean :web_filer
+      t.boolean :favicon
+      t.boolean :visitt_kort
+      t.boolean :svart_hvit_logo
+      t.boolean :separat_logo_symbol
+      t.boolean :instagram_logo
+      t.boolean :facebook_logo
+      t.boolean :backup
+      t.integer :antall_logo_forslag
+      t.decimal :pris
+      t.string :antall_endringer
+
+      t.timestamps
+    end
+  end
+end
