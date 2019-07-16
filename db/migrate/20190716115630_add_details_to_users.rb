79b06cb5be215c9b56020aed13c4f30ea4a3d393
Mose Gebreselassie <mose89@gmail.com>
2019-07-16 13:59:07 +0200

    Generate devise view for User. Add features to User.

---
+++ db/migrate/20190716115630_add_details_to_users.rb
@@ -0,0 +1,12 @@
+class AddDetailsToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :phone, :string
+    add_column :users, :business_name, :string
+    add_column :users, :org_no, :integer
+    add_column :users, :established, :integer
+    add_reference :users, :industry, foreign_key: true
+    add_column :users, :referral, :string
+  end
+end
