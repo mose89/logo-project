5570d82754a80a3663d311075c949b6e42fa5848
Mose Gebreselassie <mose89@gmail.com>
2019-07-16 13:34:49 +0200

    Add Devise. Create Packages controller with route.

--- db/migrate/20190716031356_create_join_table_product_order.rb
+++ db/migrate/20190716031356_create_join_table_product_order.rb
@@ -2,7 +2,7 @@ class CreateJoinTableProductOrder < ActiveRecord::Migration[5.2]
   def change
     create_join_table :products, :orders do |t|
       # t.index [:product_id, :order_id]
-      # t.index [:order_id, :product_id]
+       t.index [:order_id, :product_id]
     end
   end
 end
